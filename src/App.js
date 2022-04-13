d4efafe029908f90511f06211647e6a134a18e2e
Nicole <nicoleamdubois@hotmail.com>
2022-04-13 10:09:40 -0600

    dependencies

--- src/App.js
+++ src/App.js
@@ -1,32 +1,56 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    getTimeFromServer();
-  }, []);
-
-  const getTimeFromServer = async () => {
-    try {
-      let response = await fetch("/slow");
-      let vals = await response.json();
-      console.log(`vals is:`, vals);
-      return setData(vals.currentTime);
-    } catch (ex) {
-      console.log(ex);
-    }
-  };
-
+const SuperheroData = (props) => {
+ const superhero = props.superhero
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={getTimeFromServer}>Click Me</button>
-        <p>{data || "no value yet"}</p>
-      </header>
+      <div>
+        <h2>(superhero.name)</h2>
+        <p>Alter Ego:</p>
+        <p>{superhero.alterEgo}</p>
+        <p>Superpowers:</p>
+        <ul>
+          {superhero.superpowers.map((superpower) =>{
+            return <li>{superpower}</li>;
+          })}
+          <p>Superhuman strength</p> 
+          <p>Invulnerability</p>
+          </ul>
+          <p>Durability:</p>
+          <p>Indestructable</p>
+      </div>
+);
+
+
+return (
+  <div className="App" >
+    <header className= "App-header">
+      <h1>Superheroes</h1>
+    </header>
+      <SuperheroData/>
     </div>
-  );
+);
 }
+
+
+
+// function App() {
+//   const [data, setData] = useState();
+
+//   useEffect(() => {
+//     getTimeFromServer();
+//   }, []);
+
+//   const getTimeFromServer = async () => {
+//     try {
+//       let response = await fetch("/slow");
+//       let vals = await response.json();
+//       console.log(`vals is:`, vals);
+//       return setData(vals.currentTime);
+//     } catch (ex) {
+//       console.log(ex);
+//     }
+//   }; 
+// }; 
 
 export default App;
